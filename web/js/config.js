c6f3aa8bace518dc98f50e9051f183cd30c7a996
LeesonVee <weils@bsoft.com.cn>
2019-09-27 21:16:31 +0800

    重构代码

--- web/js/config.js
+++ web/js/config.js
@@ -10,6 +10,8 @@
     // let apiRootPath='http://10.32.0.169:8120/hub/';
     // let apiRootPath='http://10.195.237.115:8120/hub/';
     configpre.api_rootPath=apiRootPath;
+    configpre.api_loadModels=apiRootPath+'api/loadModels.html';
+
     configpre.map_saveMapData=apiRootPath+'map/saveMapData.html';
     configpre.map_createFile=apiRootPath+'map/createFile.html';
     configpre.map_loadAreaCodeData=apiRootPath+'map/loadAreaCodeData.html';
@@ -18,10 +20,20 @@
     configpre.map_saveModelRelation=apiRootPath+'map/saveModelRelation.html';
     configpre.api_loadColsByTableNameAndModleId=apiRootPath+'api/loadColsByTableNameAndModleId.html';
     configpre.map_queryTableDataByGisModelRelation=apiRootPath+'map/queryTableDataByGisModelRelation.html';
+    configpre.map_queryTableDataByGisModelRelationV2=apiRootPath+'map/queryTableDataByGisModelRelationV2.html';
     configpre.map_queryHeatMapDataByGisModelRelation=apiRootPath+'map/queryHeatMapDataByGisModelRelation.html';
     configpre.map_getGisIcon=apiRootPath+'map/getGisIcon.html';
     configpre.map_getOrganInfoByAreaCode=apiRootPath+'map/getOrganInfoByAreaCode.html';
     configpre.map_getMapByPkey=apiRootPath+'map/getMapByPkey.html';
+    configpre.map_loadTableDataGroupByCol=apiRootPath+'map/loadTableDataGroupByCol.html';
+    configpre.map_loadModelStyle=apiRootPath+'map/loadModelStyle.html';
+    configpre.map_saveModelStyle=apiRootPath+'map/saveModelStyle.html';
+    configpre.map_loadModelDataFloat=apiRootPath+'map/loadModelDataFloat.html';
+    configpre.map_saveModelDataFloat=apiRootPath+'map/saveModelDataFloat.html';
+    configpre.map_loadFloatDialogByRelation=apiRootPath+'map/loadFloatDialogByRelation.html';
+
+
+
 
     configpre.webGisPath='http://127.0.0.1:8140/gis/';
     // configpre.webGisPath='http://10.32.0.169:8140/gis/';
